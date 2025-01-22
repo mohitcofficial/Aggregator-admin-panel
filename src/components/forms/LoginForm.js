c5bd6db523bab77486c9e84ef721ecc2ce0f272c
mohitcofficial <mohitchandraofficial@gmail.com>
2025-01-22 13:23:34 +0530

    made login page ssr

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState } from "react";
+import ADMIN_APIs from "../../services/api/Admin.api.services";
+import classes from "./LoginForm.module.css";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Loader from "@/utils/Loader";
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (email.length === 0 || password.length === 0) {
+      toast("Fill both fields!", {
+        icon: "⚠",
+      });
+      return;
+    }
+    const body = {
+      email,
+      password,
+    };
+    setLoading(true);
+    try {
+      const data = await ADMIN_APIs.login(body);
+      toast.success(data?.message);
+      router.replace("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || "Something Went Wrong !");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      {loading && <Loader />}
+      <form className={classes.form} onSubmit={handleLogin}>
+        <p className={classes.formHeading}>Admin Panel</p>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="Enter the email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className={classes.input}
+          type="password"
+          value={password}
+          placeholder="Enter the password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={classes.loginButton} type="submit">
+          Login
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
